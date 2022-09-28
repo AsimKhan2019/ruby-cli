605db4a44283762d973fa57d024063ef1ec1632b
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-09-29 02:08:45 +0500

    Updated code

--- cli.rb
+++ cli.rb
@@ -1,13 +1,10 @@
 #ruby
 #!/usr/bin/env ruby
-
 require 'optparse'
-
 options = {}
-OptionParser.new do |parser| 
-  parser.on("-n", "--n NAME", "The name of the person") do |value|
-    options[:name] = value
-  end=
-end.parse!
-
-puts 'Hello ' + options[:name]
+OptionParser.new do |parser|
+  parser.on("-n", "--name NAME", "The name of the person") do |value|
+      options[:name] = value
+    end=
+  end.parse!
+  puts 'hello ' + options[:name]
