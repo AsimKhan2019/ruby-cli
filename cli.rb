d3d4e32344643bc7b5ce3f3b8d3a572a3e387489
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-09-29 02:06:54 +0500

    Add Option Parser

--- cli.rb
+++ cli.rb
@@ -1,2 +1,13 @@
+#ruby
 #!/usr/bin/env ruby
-puts 'Hello World!'
+
+require 'optparse'
+
+options = {}
+OptionParser.new do |parser| 
+  parser.on("-n", "--n NAME", "The name of the person") do |value|
+    options[:name] = value
+  end=
+end.parse!
+
+puts 'Hello ' + options[:name]
